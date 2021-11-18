684892019e16c8208af01a8aebcf3d6300f06ef9
Anand <anand.iiitbh@gmail.com>
2021-11-18 14:24:06 +0530

    Language Change menu done

--- app/src/main/java/com/example/mtelecom/ServicesActivity.java
+++ app/src/main/java/com/example/mtelecom/ServicesActivity.java
@@ -7,6 +7,7 @@ import static com.example.mtelecom.helper.Constants.sendWhatsappMsg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.text.SpannableString;
 import android.text.style.ImageSpan;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.Toast;
 
@@ -47,29 +49,46 @@ public class ServicesActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0, 1, 1, menuIconWithText(getResources().getDrawable(R.drawable.ic_outline_info_24), LANGUAGE_CONTEXT.getString(R.string.menu_option_about_us)));
-        menu.add(0, 2, 2, menuIconWithText(getResources().getDrawable(R.drawable.ic_outline_contact_phone_24), LANGUAGE_CONTEXT.getString(R.string.menu_option_contact_us)));
-        menu.add(0, 3, 3, menuIconWithText(getResources().getDrawable(R.drawable.ic_outline_my_location_24), LANGUAGE_CONTEXT.getString(R.string.menu_option_locate_me)));
-        menu.add(0, 4, 4, menuIconWithText(getResources().getDrawable(R.drawable.ic_outline_translate_24), LANGUAGE_CONTEXT.getString(R.string.menu_option_change_language)));
+        menu.add(0, 1, 1, menuIconWithText(getDrawable(R.drawable.ic_outline_info_24), LANGUAGE_CONTEXT.getString(R.string.menu_option_about_us)));
+        menu.add(0, 2, 2, menuIconWithText(getDrawable(R.drawable.ic_outline_contact_phone_24), LANGUAGE_CONTEXT.getString(R.string.menu_option_contact_us)));
+        menu.add(0, 3, 3, menuIconWithText(getDrawable(R.drawable.ic_outline_my_location_24), LANGUAGE_CONTEXT.getString(R.string.menu_option_locate_me)));
+
+
+        SubMenu languageMenu = menu.addSubMenu(0, 4, 4, menuIconWithText(getDrawable(R.drawable.ic_outline_translate_24), LANGUAGE_CONTEXT.getString(R.string.menu_option_change_language)));
+
+        languageMenu.clear();
+        languageMenu.add(0, 5, Menu.NONE, getString(R.string.hindi));
+        languageMenu.add(0, 6, Menu.NONE, getString(R.string.english));
+
         return true;
     }
 
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+
+        Intent intent   =   new Intent(ServicesActivity.this,
+                MainActivity.class);
+
         switch (item.getItemId()) {
             case 1:
-                Toast.makeText(ServicesActivity.this, "Profile is Clicked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ServicesActivity.this, LANGUAGE_CONTEXT.getString(R.string.menu_option_about_us), Toast.LENGTH_SHORT).show();
                 return true;
             case 2:
-                Toast.makeText(ServicesActivity.this, "Add New User is Clicked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ServicesActivity.this, LANGUAGE_CONTEXT.getString(R.string.menu_option_contact_us), Toast.LENGTH_SHORT).show();
                 return true;
             case 3:
-                Toast.makeText(ServicesActivity.this, "Switch Profile is Clicked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ServicesActivity.this, LANGUAGE_CONTEXT.getString(R.string.menu_option_locate_me), Toast.LENGTH_SHORT).show();
                 return true;
-            case 4:
-                Toast.makeText(ServicesActivity.this, "Sign Out is Clicked", Toast.LENGTH_SHORT).show();
+            case 5:
+                LocaleHelper.setLocale(ServicesActivity.this,getString(R.string.hindi_locale));
+                startActivity(intent);
+                finish();
+                return true;
+            case 6:
+                LocaleHelper.setLocale(ServicesActivity.this,getString(R.string.english_locale));
+                startActivity(intent);
+                finish();
                 return true;
         }
 
